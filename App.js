2e69e489da0f7c9ec0eae968c1fe06c102bafcf5
Emmanuel A. C. Santos <emmanuel.cavalcanti@nave.org.br>
2021-09-20 15:06:09 -0300

    update doTo List

--- App.js
+++ App.js
@@ -1,8 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Index from './src';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
-	return <Index />;
+  const [todos, setTodos] = useState([
+    /*{ text: 'Fazer café', key: '1' },
+    { text: 'Fazer um aplicativo', key: '2' },
+    { text: 'jogar ', key: '3' }*/
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    setTodos ((prevTodos) =>{
+      return [
+        {text: text, key: Math.random().toString()},
+        ...prevTodos
+      ]
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler}/>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
